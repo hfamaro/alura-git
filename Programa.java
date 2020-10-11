9e10cd637b68a6945b13e6cafbbc0edb0727260e
Heliel Amaro <hfamaro@hotmail.com>
2020-10-10 20:15:41 -0400

    Adicionei mais um print falando sobre o ATOM

--- Programa.java
+++ Programa.java
@@ -2,7 +2,8 @@ class Programa {
 
 	public static void main(String[] args) {
 
-		System.out.println("ola mundo");
+		System.out.println("ola mundo, agora estamos no Git");
+		System.out.println("Estou experimentando também o edito ATOM.");
 
 	}
 
