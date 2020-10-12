e43cd27c5709db071dde49fd28a299ade6d2a0f6
Heliel Amaro <hfamaro@hotmail.com>
2020-10-11 23:43:19 -0400

    Não gostei da forma como vc imprimiu

--- Programa.java
+++ Programa.java
@@ -3,7 +3,7 @@ class Programa {
 	public static void main(String[] args) {
 
 		System.out.println("ola mundo, agora estamos no Git");
-		System.out.println("Estou experimentando também o edito ATOM.");
+		System.out.println("Não gostei... imprime outra coisa");
 
 	}
 
