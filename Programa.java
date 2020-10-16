ff8682ede918f5d3c716c88cc40c4796a44aff55
Heliel Amaro <hfamaro@hotmail.com>
2020-10-15 20:57:06 -0400

    Revert "Comentário maroto"
    This reverts commit 5102924b249253bf3beb0c140c9ce240d9b1dfde.

--- Programa.java
+++ Programa.java
@@ -18,7 +18,6 @@ class Programa {
 	}	
 	public static int produto(int a, int b){
 		if (a == 0 || b == 0){
-			// Comentário maroto 3
 			System.out.println("A e B não podem ser 0");
 			return 0;
 		}
